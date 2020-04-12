6ce40a05e5aba36cb026860f6c4384003ca6e832
juguemseok <kalkin2@naver.comm>
2020-04-12 21:43:29 +0900

    lombok을  gradle5.0에 맞도록 수정

--- src/main/java/com/kalkin/mordenWeb/web/HelloController.java
+++ src/main/java/com/kalkin/mordenWeb/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.kalkin.mordenWeb.web;
 
 
+import com.kalkin.mordenWeb.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String Hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto HelloDto(@RequestParam("name")String name ,@RequestParam("amount")int amount){
+        return new HelloResponseDto(name,amount);
     }
 }
--- src/test/java/com/kalkin/mordenWeb/web/HelloControllerTest.java
+++ src/test/java/com/kalkin/mordenWeb/web/HelloControllerTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class HelloControllerTest {
@@ -25,6 +25,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDtoTest()throws Exception{
+        String name = "kalkin";
+        int amount = 3000;
+        mvc.perform(get("/hello/dto").param("name",name)
+                                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+
+
     }
 
 
